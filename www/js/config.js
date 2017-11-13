f237df33dd68704743ec462ab173dee5c557d76e
Lucianobr77 <luespanha@gmail.com>
2017-11-13 06:23:17 -0200

    Inicio do projeto, envio dos arquivos iniciais.

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={	
+	'ApiUrl':"https://nossopedido.com.br/mobileapp/api",
+	'DialogDefaultTitle':"Sou + Food",
+	'pushNotificationSenderid':"788287241670",
+	'facebookAppId':"433498426836152",
+	'APIHasKey' : "16n3sqyshbsfkppzyaclr50rck"
+};
